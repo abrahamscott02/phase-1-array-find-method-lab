5743d640cc43cedb00f8c05bd830a0ae2b0fc83d
Abraham Scott <abrahamscott02@gmail.com>
2023-08-28 13:08:47 -0600

    Done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const foundObject = record.find(obj => obj.result === "W");
+    console.log(foundObject);
+    if (foundObject === undefined) {
+        return undefined;
+    } else {
+        return foundObject.year;
+    }
+}
